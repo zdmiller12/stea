6be2d9c167f535ac3d43b8548dbc41e8838cee4c
scallywag12 <zdmiller12@gmail.com>
2020-04-26 14:37:49 -0400

    commit cuz i don't know whats happenng

---
+++ Problems/exercise_map.py
@@ -0,0 +1,11 @@
+"""
+EXERCISE MAP
+key   = sea  chapter _ problem location
+value = stea chapter _ problem location
+value integer will be converted to 0-left-padded string when appropriate
+"""
+
+EXERCISE_MAP = {}
+EXERCISE_MAP["sea-08-01"] = 2
+EXERCISE_MAP["sea-08-02"] = 3
+
